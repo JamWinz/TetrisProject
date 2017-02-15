6d518db5d567cb6b57b05e678625e88d63906417
JamWinz <jhwinya@PointPark.EDU>
2017-02-14 19:59:08 -0500

    Add files via upload

--- tetris.js
+++ tetris.js
@@ -1,5 +1,11 @@
 var tetris = (function() {
   console.log("Initilizing tetris.js");
+
+  var block = [[true, true],
+               [true, true]
+              ];
+
+
   var grid = [[null, null, null, null, null, null, null, null, null, null],
               [null, null, null, null, null, null, null, null, null, null],
               [null, null, null, null, null, null, null, null, null, null],
@@ -17,7 +23,33 @@ var tetris = (function() {
               [null, null, null, null, null, null, null, null, null, null]
             ];
 
-            //var player = "X";
+            //Box Shape
+            /*
+            grid[row][col+1] = false;
+            grid[row][col] = false;
+            grid[row+1][col] = false;
+            grid[row+1][col+1] = false;
+            row++;
+            grid[row][col+1] = true;
+            grid[row][col] = true;
+            grid[row+1][col] = true;
+            grid[row+1][col+1] = true;
+            */
+
+            //Line
+            /*
+            grid[row][col+1] = false;
+            grid[row][col] = false;
+            row++;
+            grid[row][col+1] = true;
+            grid[row][col] = true;
+            */
+
+            function randomPiece(arr) {
+              var rng = Math.floor(Math.random() * 2);
+
+              return rng;
+            }
 
             function playMove(id){
               var row = parseInt(id[1], 10);
@@ -28,12 +60,12 @@ var tetris = (function() {
                 grid[row][col] = false;
                 row++;
                 grid[row][col] = true;
-                if (row === grid.length-1) {
+                if (row === grid.length-1 || grid[row+1][col] === true) {
                   // we reached the bottom
                   clearInterval(interval);
                 }
                 notify();
-              }, 500);
+              }, 100);
         }
 
             var listeners = [];
