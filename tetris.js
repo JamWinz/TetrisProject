b84b0e5f602013f99d1a53994d1ec0432c54aaeb
JamWinz <jhwinya@PointPark.EDU>
2017-02-14 19:05:34 -0500

    Add files via upload
    Updated In Class

--- tetris.js
+++ tetris.js
@@ -22,37 +22,25 @@ var tetris = (function() {
             function playMove(id){
               var row = parseInt(id[1], 10);
               var col = parseInt(id[3], 10);
-              var count = row;
-              //grid[count][col] //= player;
-              /*setInterval(function() {
-              }, 1000);*/
-
-              //setTimeout(function() {
-              //}, 500);
-            for(var i = row; i < 15; i++){
-              $("#R" + count + "C" + col).queue(function () {
-                console.log("Back into the QUEUE function");
-              $("#R" + count + "C" + col).removeClass("colorBox").delay(500);
-              console.log("#R" + count + "C" + col);//Print the cell that we are currently on
-              console.log("Executed removeClass!");
-              count++;
-              console.log(count);
-              $("#R" + count + "C" + col).addClass("colorBox").delay(500);
-              console.log("Executed addClass!")
-              //Count here creates full rows of blocks
-              });
-            }
-              console.log("Out of the loop..");
-              //setInterval( function() {
-
-              //}, 500 );
+              grid[row][col] = true;
               notify();
+              var interval = setInterval(function() {
+                grid[row][col] = false;
+                row++;
+                grid[row][col] = true;
+                if (row === grid.length-1) {
+                  // we reached the bottom
+                  clearInterval(interval);
+                }
+                notify();
+              }, 500);
         }
 
             var listeners = [];
 
             function addListener(cb) {
               listeners.push(cb);
+              notify();
             }
 
             function notify() {
